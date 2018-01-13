f16d6d02beeb81f1952edea849ff6ca41be422db
YanYi2017 <34807478+YanYi2017@users.noreply.github.com>
2018-01-13 20:20:54 +0800

    成功编写测试用例来保证菜单元素默认是隐藏的

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -51,9 +51,7 @@ $(function() {
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
          it('should be able to make sure the menu element is hidden by default', function() {
-            //var slideMenu = document.getElementsByClassName('slide-menu');
-            //expect(slideMenu[0].getAttribute('transform')).not.toBe(null);
-            expect($('.menu-hidden .slide-menu').attr('transform')).toBeDefined();
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
          });
 
          /* TODO:
