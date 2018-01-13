0dcf73ef88f0f6fbe530ea444dd085a8731ce614
YanYi2017 <34807478+YanYi2017@users.noreply.github.com>
2018-01-13 19:34:24 +0800

    写了一个测试用例保证菜单元素默认是隐藏的

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,17 +45,24 @@ $(function() {
 
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
-
+    describe('The menu', function() {
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
+         it('should be able to make sure the menu element is hidden by default', function() {
+            //var slideMenu = document.getElementsByClassName('slide-menu');
+            //expect(slideMenu[0].getAttribute('transform')).not.toBe(null);
+            expect($('.menu-hidden .slide-menu').attr('transform')).toBeDefined();
+         });
 
          /* TODO:
           * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
           * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
+    });
+
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
 
